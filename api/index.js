cb0d3e50c5b95e8e36d9c7f04c2ab72d335ff6dc
HarshVardhan2025 <156245029+HarshVardhan2025@users.noreply.github.com>
2025-02-24 23:14:02 +0530

    for deployment

--- api/index.js
+++ api/index.js
@@ -512,4 +512,7 @@ app.post('/admin/places', async (req, res) => {
     }
 });
 
-app.listen(4000, () => console.log("Server running on port 4000"));
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
